98b53f56f851117fe22622dbc3675a6e341395e3
zocha_sz <zofia.szczudlik@gmail.com>
2017-09-30 18:19:20 +0200

    feat: update portfolio components

--- src/app/components/portfolio/portfolio-new/portfolio-new.component.ts
+++ src/app/components/portfolio/portfolio-new/portfolio-new.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Project } from './../../../shared/models/project.model';
+import { Letter } from './../../../shared/models/letter.model';
+import { Portfolio } from './../../../shared/models/portfolio.model';
+import { PortfolioService } from './../../../shared/services/portfolio.service';
+
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-portfolio-new',
@@ -6,10 +14,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio-new.component.css']
 })
 export class PortfolioNewComponent implements OnInit {
+  portfolio: Portfolio = new Portfolio();
+  projects: Array<Project>;
+  letters: Array<Letter>;
+  error: string;
 
-  constructor() { }
+  constructor( private portfolioService: PortfolioService, private router: Router ) { }
 
   ngOnInit() {
+    this.fetchProjectsList(),
+    this.fetchLettersList()
   }
-
+  fetchProjectsList() {
+    this.portfolioService.getProjects().subscribe(
+      projects => this.projects = projects,
+      error => console.log(error)
+    );
+  }
+  fetchLettersList() {
+    this.portfolioService.getLetters().subscribe(
+      letters => this.letters = letters,
+      error => console.log(error)
+    );
+  }
+  onSubmitNew(newForm): void {
+    console.log(newForm.value);
+    this.portfolioService.create(this.portfolio).subscribe(
+      () => {
+        newForm.reset();
+        this.router.navigate(['/dashboard']);
+      },
+      (error) => {this.error = error}
+    )
+  }
 }
