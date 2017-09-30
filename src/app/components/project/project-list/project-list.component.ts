bb99f8bd06f83ec3ce93c42d0b43f0fd23c5a8c8
zocha_sz <zofia.szczudlik@gmail.com>
2017-09-30 12:04:53 +0200

    feat: update project component

--- src/app/components/project/project-list/project-list.component.ts
+++ src/app/components/project/project-list/project-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Project } from './../../../shared/models/project.model';
+import { ProjectService } from './../../../shared/services/project.service'
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-project-list',
@@ -6,10 +11,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-list.component.css']
 })
 export class ProjectListComponent implements OnInit {
+  projects: Array<Project> = [];
+  project: Project;
 
-  constructor() { }
+  constructor( private projectService: ProjectService, private router: Router ) { }
 
   ngOnInit() {
+    this.fetchList();
   }
 
+  fetchList() {
+    this.projectService.getAll().subscribe(
+      projects => this.projects = projects,
+      error => console.log(error)
+    );
+  }
+  addProject() {
+    this.router.navigate(['/project/new']);
+  }
+  showProject(id: string) {
+    this.router.navigate([`/project/${id}`]);
+  }
+  delete(id: string) {
+    this.projectService.remove(id)
+    .subscribe((removed) => {
+      if (removed) {
+        this.projects = this.projects.filter(project => project._id !== id)
+      } else {
+        console.log ("error: List not found")
+      }
+    },
+    (err) => console.log(err)
+    )
+  }
 }
+
+// export class ListComponent implements OnInit {
+//
+//
+//   ngOnInit() { }
+//
+//   removeList() {
+//     this.listService.remove(this.list._id)
+//       .subscribe((removed) => {
+//         if (removed) {
+//           this.onDelete.emit(this.list)
+//         } else {
+//           this.toastService.onError({ message: 'List not found'})
+//         }
+//       },
+//       (err) => this.toastService.onError(err)
+//     );
+//   }
+//
+//   editList() {
+//     this.listService.edit(this.list).subscribe(
+//       (list) => {
+//         if (list) {
+//           this.list = list;
+//           this.onEdit.emit(this.list);
+//         } else {
+//           this.toastService.onError({ message: 'List not found' })
+//         }
+//       },
+//       (err) => this.toastService.onError(err)
+//     );
+//   }
+//   addCard(title) {
+//     // TODO: Implement
+//   }
+// }
--- src/app/components/project/project-new/project-new.component.ts
+++ src/app/components/project/project-new/project-new.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Project } from './../../../shared/models/project.model';
+import { ProjectService } from './../../../shared/services/project.service'
 
 @Component({
   selector: 'app-project-new',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-new.component.css']
 })
 export class ProjectNewComponent implements OnInit {
+  project: Project = new Project;
+  error: string;
 
-  constructor() { }
+  constructor( private projectService: ProjectService, private router: Router ) { }
 
   ngOnInit() {
   }
 
+  onSubmitNew(newForm): void {
+
+    this.projectService.create(newForm.value).subscribe(
+      () => {
+        newForm.reset();
+        this.router.navigate(['/dashboard']);
+      },
+      (error) => {this.error = error}
+    )
+  }
 }
