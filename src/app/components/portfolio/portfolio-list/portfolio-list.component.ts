70a9b25d3a34c6f8612163fd5285da638ac69534
zocha_sz <zofia.szczudlik@gmail.com>
2017-09-30 12:04:27 +0200

    feat: update portfolio component

--- src/app/components/portfolio/portfolio-list/portfolio-list.component.ts
+++ src/app/components/portfolio/portfolio-list/portfolio-list.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Portfolio } from './../../../shared/models/portfolio.model';
+import { PortfolioService } from './../../../shared/services/portfolio.service'
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-portfolio-list',
   templateUrl: './portfolio-list.component.html',
-  styleUrls: ['./portfolio-list.component.css']
+  styleUrls: [
+    './../../project/project-list/project-list.component.css',
+  './portfolio-list.component.css'
+]
 })
 export class PortfolioListComponent implements OnInit {
+  portfolios: Array<Portfolio> = [];
 
-  constructor() { }
+  constructor( private portfolioService: PortfolioService, private router: Router ) { }
 
   ngOnInit() {
+    this.fetchList();
   }
 
+  fetchList() {
+    this.portfolioService.getAll().subscribe(
+      portfolios => this.portfolios = portfolios,
+      error => console.log(error)
+    );
+  }
+  addProject() {
+    this.router.navigate(['/portfolio/new']);
+  }
 }
